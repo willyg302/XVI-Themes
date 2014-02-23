36ea37dc2c332296a0c78a2b5093ecec85a30330
William Gaul <willyg302@gmail.com>
2014-02-23 12:51:25 -1000

    Upload theme site

---
+++ themes.js
@@ -0,0 +1,46 @@
+var themes = {};
+
+themes.default = {
+	name: 'DEFAULT',
+	author: 'William Gaul',
+	colors: [
+		'#1d1f21', '#282a2e', '#373b41', '#969896', '#b4b7b4', '#c5c8c6', '#e0e0e0', '#ffffff',
+		'#c82829', '#f5871f', '#eab700', '#718c00', '#3e999f', '#4271ae', '#8959a8', '#a3685a'
+	]
+};
+
+themes.chalk = {
+	name: 'Chalk',
+	author: 'Chris Kempson',
+	colors: [
+		'#151515', '#202020', '#303030', '#505050', '#b0b0b0', '#d0d0d0', '#e0e0e0', '#f5f5f5',
+		'#fb9fb1', '#eda987', '#ddb26f', '#acc267', '#12cfc0', '#6fc2ef', '#e1a3ee', '#deaf8f'
+	]
+};
+
+themes.mocha = {
+	name: 'Mocha',
+	author: 'Chris Kempson',
+	colors: [
+		'#3b3228', '#534636', '#645240', '#7e705a', '#b8afad', '#d0c8c6', '#e9e1dd', '#f5eeeb',
+		'#cb6077', '#d28b71', '#f4bc87', '#beb55b', '#7bbda4', '#8ab3b5', '#a89bb9', '#bb9584'
+	]
+};
+
+themes.solarized = {
+	name: 'Solarized',
+	author: 'Ethan Schoonover',
+	colors: [
+		'#002b36', '#073642', '#586e75', '#657b83', '#839496', '#93a1a1', '#eee8d5', '#fdf6e3',
+		'#dc322f', '#cb4b16', '#b58900', '#859900', '#2aa198', '#268bd2', '#6c71c4', '#d33682'
+	]
+};
+
+themes.tomorrow = {
+	name: 'Tomorrow',
+	author: 'Chris Kempson',
+	colors: [
+		'#1d1f21', '#282a2e', '#373b41', '#969896', '#b4b7b4', '#c5c8c6', '#e0e0e0', '#ffffff',
+		'#cc6666', '#de935f', '#f0c674', '#b5bd68', '#8abeb7', '#81a2be', '#b294bb', '#a3685a'
+	]
+};
